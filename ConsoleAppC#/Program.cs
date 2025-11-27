5f992793b159790e0e18a40ba2ba9f27b54881c4
Ali Tariq <alibabapk@aol.com>
2025-11-27 17:27:50 +0500

    Sealed it

--- ConsoleAppC#/Program.cs
+++ ConsoleAppC#/Program.cs
@@ -1,19 +1,39 @@
-﻿namespace ConsoleAppCS
+﻿using Humanizer;
+
+namespace ConsoleAppCS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Program starts...");
+            Console.WriteLine("==============");
             //CollectionsPractice collectionsPractice = new CollectionsPractice();
             //collectionsPractice.DoPractice();
             //ValueTypeExample.Maine();
             //NullableType.Maine();
             //SomeQueues.Maine();
             //ConcurrentBagDemo.Man();
-            Example.Man();
-            Console.WriteLine("Program ends...");
+            //Example.Man();
+            SealIt.RunSealedLogic();
+            //HumanizeQuantities();
+            //HumanizeDates();
+            Console.WriteLine("==============");
         }
+
+        static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
     }
 
 }
---
+++ ConsoleAppC#/SealIt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCS
+{
+    internal sealed class SealIt
+    {
+        class X
+        {
+            protected virtual void F() { Console.WriteLine("X.F"); }
+            protected virtual void F2() { Console.WriteLine("X.F2"); }
+
+            public virtual void RunFunX()
+            {
+                this.F();
+                this.F2();
+            }
+        }
+
+        class Y : X
+        {
+            sealed protected override void F() { Console.WriteLine("Y.F"); }
+            protected override void F2() { Console.WriteLine("Y.F2"); }
+
+             public virtual void RunFunY()
+            {
+                this.F();
+                this.F2();
+            }
+        }
+
+        class Z : Y
+        {
+            // Attempting to override F causes compiler error CS0239.
+            // protected override void F() { Console.WriteLine("Z.F"); }
+
+            // Overriding F2 is allowed.
+            protected override void F2() { Console.WriteLine("Z.F2"); }
+
+            public virtual void RunFunZ()
+            {
+                this.F();
+                this.F2();
+            }
+        }
+
+        public static void RunSealedLogic()
+        {
+            var X = new X();
+            X.RunFunX();
+        }
+    }
+}
